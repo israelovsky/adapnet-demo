022ba8a77e397bead91c71887ff37d2f5250da6f
israelovsky <israel.ramirez.ayon@gmail.com>
2014-01-18 19:40:41 -0600

    First Commit

---
+++ cliente/js/login.js
@@ -0,0 +1,6 @@
+window.userName = '';
+
+
+$( document ).ready(function() {
+   
+});
---
+++ cliente/js/main.js
@@ -0,0 +1,10 @@
+window.numPendientes = 0;
+window.numAsignacion = 0;
+window.numRequisicion= 0;
+window.numDocumentos = 0;
+
+
+$("#numPendientes").text(window.numPendientes);
+$("#numAsignacion").text(window.numPendientes);
+$("#numRequisicion").text(window.numPendientes);
+$("#numDocumentos").text(window.numPendientes);
