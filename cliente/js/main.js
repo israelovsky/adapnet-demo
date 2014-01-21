6fa99d43ffea2d0668ccb2338f3d7d6f60bc97f0
israelovsky <israel.ramirez.ayon@gmail.com>
2014-01-20 18:28:09 -0600

    Asignación botón
    Cambio Etiqueta

--- cliente/js/main.js
+++ cliente/js/main.js
@@ -3,7 +3,6 @@ window.numAsignacion = 0;
 window.numRequisicion= 0;
 window.numDocumentos = 0;
 
-
 $("#numPendientes").text(window.numPendientes);
 $("#numAsignacion").text(window.numPendientes);
 $("#numRequisicion").text(window.numPendientes);
